dcf559870d32c322f9728cdae8732ae86de4262e
YaldaGheisi <89938223+YaldaGheisi@users.noreply.github.com>
2023-02-02 10:56:04 -0600

    feb1#

--- CovidEvent/CovidEvent/settings.py
+++ CovidEvent/CovidEvent/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'CovidEvent.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.spostgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- CovidEvent/CovidEvent/urls.py
+++ CovidEvent/CovidEvent/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('events/',include('events.urls'))
+    path('events/',include('events.urls')),
     path('admin/', admin.site.urls),
 ]
